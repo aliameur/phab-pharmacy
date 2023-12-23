60f04f5d771bbb28fbf22d6b92a3f531f6f82650
aliameur <aliameur169@gmail.com>
2023-12-23 22:59:28 +0100

    add left hero section

--- apps/online-store/app/page.tsx
+++ apps/online-store/app/page.tsx
@@ -2,9 +2,11 @@ export default async function Index() {
   return (
     <main>
       <header className="flex min-h-[calc(100svh-96px)]">
-        <div className="text-mineral-green-600 flex w-1/2 flex-col gap-8 px-16 pb-[72px]">
-          <h1 className="text-5xl font-merriweather">Your One-Stop Online Wellness Shop</h1>
-          <p>
+        <div className="text-mineral-green-600 flex w-1/2 flex-col justify-center gap-8 px-16 pb-[72px]">
+          <h1 className="font-merriweather text-5xl">
+            Your One-Stop Online Wellness Shop
+          </h1>
+          <p className="text-xl">
             Find all your healthcare needs with ease. Just type and search
             below.
           </p>
