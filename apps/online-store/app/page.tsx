4302dcfa6e9a9e830ff847e75f1a125726d35b9e
aliameur <aliameur169@gmail.com>
2023-12-23 22:57:28 +0100

    add merriweather font

--- apps/online-store/app/page.tsx
+++ apps/online-store/app/page.tsx
@@ -1,3 +1,16 @@
 export default async function Index() {
-  return <div>home</div>;
+  return (
+    <main>
+      <header className="flex min-h-[calc(100svh-96px)]">
+        <div className="text-mineral-green-600 flex w-1/2 flex-col gap-8 px-16 pb-[72px]">
+          <h1 className="text-5xl font-montserrat">Your One-Stop Online Wellness Shop</h1>
+          <p>
+            Find all your healthcare needs with ease. Just type and search
+            below.
+          </p>
+        </div>
+        <div className="w-1/2"></div>
+      </header>
+    </main>
+  );
 }
