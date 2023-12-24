bb171f6f75c67d5462f6b7c3594c17ffa9a986bf
aliameur <aliameur169@gmail.com>
2023-12-24 01:24:56 +0100

    add carousel logic

--- apps/online-store/app/page.tsx
+++ apps/online-store/app/page.tsx
@@ -1,9 +1,10 @@
+import { Carousel } from '../components/carousel';
 import { Search } from '../components/search';
 
 export default async function Index() {
   return (
     <main>
-      <header className="flex min-h-[calc(100svh-96px)]">
+      <header className="flex h-[calc(100svh-96px)]">
         <div className="text-mineral-green-600 flex w-1/2 flex-col justify-center gap-8 px-16 pb-[72px]">
           <h1 className="font-merriweather text-5xl">
             Your One-Stop Online Wellness Shop
@@ -14,7 +15,13 @@ export default async function Index() {
           </p>
           <Search />
         </div>
-        <div className="w-1/2"></div>
+        <div className="h-full flex w-1/2 flex-col">
+          <div className="flex h-[calc(100%-72px)] grow gap-4 bg-red-500">
+            <Carousel direction="up" offset={-50} />
+            <Carousel direction="down" />
+          </div>
+          <div className="bg-mineral-green-600 shrink-0 h-[72px]" />
+        </div>
       </header>
     </main>
   );
