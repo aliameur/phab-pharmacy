f6fb0bd453ff20b99eb83f2170363de771c7c099
aliameur <aliameur169@gmail.com>
2023-12-24 16:15:27 +0100

    change nav links to use Link component

--- apps/online-store/components/nav.tsx
+++ apps/online-store/components/nav.tsx
@@ -1,30 +1,32 @@
-import { ReactNode } from 'react';
+import { ComponentProps, ReactNode } from 'react';
 
 import { Logo } from './logo';
+import Link from 'next/link';
 
 export const Nav = () => {
   return (
     <nav className="flex h-24 w-full items-center justify-between px-16">
       <div className="flex items-center gap-8">
         <Logo />
-        <NavLink>Cold and Flu</NavLink>
-        <NavLink>Skin Care</NavLink>
-        <NavLink>Headaches and Pain Relief</NavLink>
-        <NavLink>All Products</NavLink>
+        <NavLink href="/">Cold and Flu</NavLink>
+        <NavLink href="/">Skin Care</NavLink>
+        <NavLink href="/">Headaches and Pain Relief</NavLink>
+        <NavLink href="/">All Products</NavLink>
       </div>
       <div></div>
     </nav>
   );
 };
 
-type TNavLink = {
+type TNavLink = ComponentProps<typeof Link> & {
   children: ReactNode;
 };
-const NavLink = ({ children }: TNavLink) => {
+const NavLink = ({ children, ...props }: TNavLink) => {
   return (
-    <div className="text-mineral-green-600 group relative px-4 py-2">
+    <Link {...props} className="text-mineral-green-600 group relative px-4 py-2">
       {children}
-      <div className="bg-mineral-green-600 absolute bottom-2 left-4 right-4 h-0.5 origin-center translate-y-full scale-x-0 transition-all duration-300 group-hover:scale-x-100" />
-    </div>
+      <div
+        className="bg-mineral-green-600 absolute bottom-2 left-4 right-4 h-0.5 origin-center translate-y-full scale-x-0 transition-all duration-300 group-hover:scale-x-100" />
+    </Link>
   );
 };
