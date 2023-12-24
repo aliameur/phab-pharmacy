f19400e06f0e3a59eeb382f7149cbd325669a074
aliameur <aliameur169@gmail.com>
2023-12-24 17:09:39 +0100

    feat(online-store): add hover effect to button.tsx

--- apps/online-store/components/button.tsx
+++ apps/online-store/components/button.tsx
@@ -9,10 +9,20 @@ export const Button = ({
 }: ComponentProps<'button'>) => {
   return (
     <button
-      className={cn('text-pampas-100 bg-mineral-green-600 rounded-2xl px-8 py-4', className)}
+      className={cn(
+        'text-pampas-100 bg-mineral-green-600 group rounded-2xl px-8 py-4',
+        className,
+      )}
       {...props}
     >
-      {children}
+      <div className="relative overflow-hidden">
+        <span className="flex transition-all duration-300 group-hover:-translate-y-full">
+          {children}
+        </span>
+        <span className="absolute inset-0 flex translate-y-full transition-all duration-300 group-hover:translate-y-0">
+          {children}
+        </span>
+      </div>
     </button>
   );
 };
