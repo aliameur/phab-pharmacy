dc0b2864e7abf9ec9386748217498a65bfdf1815
aliameur <aliameur169@gmail.com>
2023-12-24 01:29:07 +0100

    chore: remove react native packages

--- apps/mobile-app/src/app/App.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react-native';
-import React from 'react';
-
-import App from './App';
-
-test('renders correctly', () => {
-  const { getByTestId } = render(<App />);
-  expect(getByTestId('heading')).toHaveTextContent('Welcome');
-});
