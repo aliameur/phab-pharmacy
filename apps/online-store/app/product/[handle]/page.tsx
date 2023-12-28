66fb75bdbd8b86490ba4645d734f2e49150be70e
Ali Ameur <aliameur169@gmail.com>
2023-12-28 06:03:13 +0100

    feat(online-store): add reviews and details to product page

--- apps/online-store/app/product/[handle]/page.tsx
+++ apps/online-store/app/product/[handle]/page.tsx
@@ -1,12 +1,14 @@
 import { AddToCart } from '../../../components/add-to-cart';
-import { Rating } from '../../../components/rating';
+import { DetailSection } from '../../../components/detail-section';
 import { Gallery } from '../../../components/gallery';
+import { Rating } from '../../../components/rating';
+import { Review } from '../../../components/review';
 
 export default async function Page() {
   return (
     <main className="px-16">
-      <div className="flex pt-12">
-        <Gallery className="h-full w-1/2 pr-10"/>
+      <div className="flex pb-16 pt-12">
+        <Gallery className="sticky top-32 h-full w-1/2 pr-10" />
         <div className="flex w-1/2 flex-col gap-8 pl-10">
           <div className="flex flex-col gap-2 text-mineral-green-600">
             <h2 className="font-merriweather text-3xl font-bold">BIOGENA</h2>
@@ -35,7 +37,44 @@ export default async function Page() {
             </p>
           </div>
           <AddToCart />
+          <div className="flex flex-col gap-8 pt-8">
+            <DetailSection title="Description">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. At
+              doloribus exercitationem nostrum pariatur? Amet, aperiam deserunt
+              maxime omnis perspiciatis suscipit.
+            </DetailSection>
+            <DetailSection title="Composition">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. At
+              doloribus exercitationem nostrum pariatur? Amet, aperiam deserunt
+              maxime omnis perspiciatis suscipit.
+            </DetailSection>
+            <DetailSection title="Ingredients">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. At
+              doloribus exercitationem nostrum pariatur? Amet, aperiam deserunt
+              maxime omnis perspiciatis suscipit.
+            </DetailSection>
+            <DetailSection title="Usage">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. At
+              doloribus exercitationem nostrum pariatur? Amet, aperiam deserunt
+              maxime omnis perspiciatis suscipit.
+            </DetailSection>
+          </div>
         </div>
+      </div>
+      <div className="border-t border-mineral-green-600/20">
+        {Array.from({ length: 4 }).map((_, i) => (
+          <Review
+            key={i}
+            rating={3.5}
+            name="Ali Ameur"
+            date={new Date()}
+            likes={23}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. At
+            doloribus exercitationem nostrum pariatur? Amet, aperiam deserunt
+            maxime omnis perspiciatis suscipit.
+          </Review>
+        ))}
       </div>
     </main>
   );
