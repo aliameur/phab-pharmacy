eda483f06cc08a15d6b6134a7f71382111d86691
Ali Ameur <aliameur169@gmail.com>
2024-01-02 18:29:29 +0100

    fix(online-store): remove unused util

--- apps/online-store/lib/utils.ts
+++ apps/online-store/lib/utils.ts
@@ -4,21 +4,3 @@ import { twMerge } from 'tailwind-merge';
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
 };
-
-export const objectToURLSearchParams = (
-  obj: Record<string, never>,
-): URLSearchParams => {
-  const params = new URLSearchParams();
-
-  for (const key in obj) {
-    if (Array.isArray(obj[key])) {
-      (obj[key] as []).forEach((value) => {
-        params.append(`${key}[]`, value);
-      });
-    } else {
-      params.append(key, obj[key]);
-    }
-  }
-
-  return params;
-};
