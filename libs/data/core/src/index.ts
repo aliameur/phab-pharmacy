582d0f3d96ec52c0b6690c41c4edcbfb6c7d858d
Ali Ameur <aliameur169@gmail.com>
2024-01-03 16:44:18 +0100

    refactor(data): update API to export new function

--- libs/data/core/src/index.ts
+++ libs/data/core/src/index.ts
@@ -1,4 +1,7 @@
 export * from './lib/get-products';
 export * from './lib/get-product-by-handle';
+
 export * from './lib/get-categories';
+export * from './lib/get-category-by-handle';
+
 export type { TRequestFn } from './types';
--- libs/data/react-native/src/lib/reexport.ts
+++ libs/data/react-native/src/lib/reexport.ts
@@ -5,3 +5,4 @@ import { medusaFetch } from './medusa-fetch';
 export const getProducts = Core.getProducts(medusaFetch);
 export const getProductByHandle = Core.getProductByHandle(medusaFetch);
 export const getCategories = Core.getCategories(medusaFetch);
+export const getCategoryByHandle = Core.getCategoryByHandle(medusaFetch);
