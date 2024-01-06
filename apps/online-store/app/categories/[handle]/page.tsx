99e41ed917bb95e93f20e658fcfb1d0e02e7c0fd
Ali Ameur <aliameur169@gmail.com>
2024-01-06 08:05:14 +0000

    feat(online-store): add product-card

--- apps/online-store/app/categories/[handle]/page.tsx
+++ apps/online-store/app/categories/[handle]/page.tsx
@@ -1,11 +1,11 @@
-import Image from 'next/image';
-import Link from 'next/link';
 import { notFound } from 'next/navigation';
 
 import {
   getCategoryByHandle,
   getProductsByCategoryHandle,
 } from '@phab/data-next';
+
+import { ProductCard } from '../../../components/product-card';
 
 type Props = {
   params: { handle: string };
@@ -23,47 +23,24 @@ export default async function Page({ params: { handle } }: Props) {
       <div className="bg-pattern flex h-72 w-full items-center justify-center text-pampas-100">
         <h2 className="font-merriweather text-4xl">{category.name}</h2>
       </div>
-      <div className="px-4 py-4 md:px-16">
-        <div className="grid grid-flow-row grid-cols-3 gap-4">
+      <div className="px-4 py-4 lg:px-16">
+        <div className="grid grid-flow-row grid-cols-1 gap-4 min-[488px]:grid-cols-2 lg:grid-cols-3">
           {products.map((product) => (
-            <Link href={`/products/${product.handle}`} key={product.id} className="relative transition-opacity">
-              <div className="absolute left-0 top-0 z-0 aspect-square w-full bg-mineral-green-600" />
-              <svg
-                className="absolute inset-0 z-10 translate-x-4"
-                xmlns="http://www.w3.org/2000/svg"
-                width="276"
-                height="100%"
-                viewBox="0 0 276 351"
-                fill="none"
-              >
-                <path
-                  d="M203.361 39.3611L230.444 0H275.222V351H0.777344L203.361 39.3611Z"
-                  fill="#BFD0CC"
-                />
-              </svg>
-              <div className="relative z-20 aspect-square overflow-hidden">
-                <Image
-                  className="object-contain"
-                  fill
-                  // src={product.thumbnail || ''}
-                  src={'/placeholder-product.png'}
-                  alt={product.title || ''}
-                />
-              </div>
-              <div className="relative z-30 pb-4 text-center">
-                <h4 className="text-lg font-bold text-mineral-green-600">
-                  {product.title}
-                </h4>
-                <p className="text-mineral-green-600">
-                  £{' '}
-                  {product.variants.reduce((cheapest, variant) =>
-                    cheapest.prices[0].amount < variant.prices[0].amount
-                      ? cheapest
-                      : variant,
-                  ).prices[0].amount / 100}
-                </p>
-              </div>
-            </Link>
+            <ProductCard
+              key={product.id}
+              href={`/products/${product.handle}`}
+              alt={product.title || ''}
+              // src={product.thumbnail || ''}
+              src="/placeholder-product.png"
+              title={product.title || ''}
+              price={
+                product.variants.reduce((cheapest, variant) =>
+                  cheapest.prices[0].amount < variant.prices[0].amount
+                    ? cheapest
+                    : variant,
+                ).prices[0].amount / 100
+              }
+            />
           ))}
         </div>
       </div>
---
+++ apps/online-store/components/product-card/index.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { SVGPattern } from './svg-pattern';
+
+type TProductCard = {
+  href: string;
+  src: string;
+  alt: string;
+  title: string;
+  price: number;
+};
+
+export const ProductCard = ({ href, src, alt, title, price }: TProductCard) => {
+  return (
+    <Link href={href} className="relative transition-opacity">
+      <div className="absolute left-0 top-0 z-0 aspect-square w-full bg-mineral-green-600" />
+      <SVGPattern className="absolute -right-0 top-0 z-0 block h-full w-full" />
+      <div className="relative z-0 aspect-square overflow-hidden">
+        <Image className="object-contain" fill src={src} alt={alt} />
+      </div>
+      <div className="relative z-30 h-20 text-center">
+        <h4 className="text-lg font-bold text-mineral-green-600">{title}</h4>
+        <p className="text-mineral-green-600">£ {price}</p>
+      </div>
+    </Link>
+  );
+};
