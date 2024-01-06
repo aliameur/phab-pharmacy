a5c61fa7e65661d01cffbfba8bd336b8a086ac08
Ali Ameur <aliameur169@gmail.com>
2024-01-06 14:18:33 +0000

    feat(online-store): refactor product grid for consistency, and placeholders for search and filtering

--- apps/online-store/app/categories/[handle]/page.tsx
+++ apps/online-store/app/categories/[handle]/page.tsx
@@ -5,7 +5,7 @@ import {
   getProductsByCategoryHandle,
 } from '@phab/data-next';
 
-import { ProductCard } from '../../../components/product-card';
+import { ProductGrid } from '../../../components/product-grid';
 
 type Props = {
   params: { handle: string };
@@ -24,25 +24,7 @@ export default async function Page({ params: { handle } }: Props) {
         <h2 className="font-merriweather text-4xl">{category.name}</h2>
       </div>
       <div className="px-4 py-4 lg:px-16">
-        <div className="grid grid-flow-row grid-cols-1 gap-4 min-[488px]:grid-cols-2 lg:grid-cols-3">
-          {products.map((product) => (
-            <ProductCard
-              key={product.id}
-              href={`/products/${product.handle}`}
-              alt={product.title || ''}
-              // src={product.thumbnail || ''}
-              src="/placeholder-product.png"
-              title={product.title || ''}
-              price={
-                product.variants.reduce((cheapest, variant) =>
-                  cheapest.prices[0].amount < variant.prices[0].amount
-                    ? cheapest
-                    : variant,
-                ).prices[0].amount / 100
-              }
-            />
-          ))}
-        </div>
+        <ProductGrid products={products} />
       </div>
     </div>
   );
--- apps/online-store/app/store/page.tsx
+++ apps/online-store/app/store/page.tsx
@@ -2,8 +2,8 @@ import { notFound } from 'next/navigation';
 
 import { getProducts } from '@phab/data-next';
 
-import { ProductCard } from '../../components/product-card';
-import { getCheapestVariantPriceInCurrency } from '../../lib/utils';
+import { ProductGrid } from '../../components/product-grid';
+import { Search } from '../../sections/hero/search';
 
 export default async function Page() {
   const products = await getProducts().catch((err) => {
@@ -13,31 +13,31 @@ export default async function Page() {
   return (
     <div>
       <div className="bg-pattern flex h-72 w-full items-center justify-center text-pampas-100">
-        <h2 className="font-merriweather text-4xl">All Products</h2>
+        <Search className="w-1/2" />
       </div>
       <div className="px-4 py-4 lg:px-16">
-        <div className="grid grid-flow-row grid-cols-1 gap-4 min-[488px]:grid-cols-2 md:gap-8 lg:grid-cols-3">
-          {products.map((product) => {
-            const cheapestPrice = getCheapestVariantPriceInCurrency(
-              product.variants,
-              'EUR',
-            );
-            return (
-              <ProductCard
-                key={product.id}
-                href={`/products/${product.handle}`}
-                alt={product.title || ''}
-                // src={product.thumbnail || ''}
-                src="/placeholder-product.png"
-                title={product.title || ''}
-                price={{
-                  amount: cheapestPrice?.amount,
-                  currencyCode: cheapestPrice?.currency_code,
-                }}
-              />
-            );
-          })}
+        <div className="flex justify-between pb-8 pt-4">
+          <div className="text-mineral-green-600">
+            <p className="text-xl">
+              Search results for &quot;search term&quot;
+            </p>
+            <p className="text-sm">
+              Found 3 results for &quot;search term&quot;
+            </p>
+          </div>
+          <div className="flex gap-4 ">
+            <button className="cursor-not-allowed bg-mineral-green-600 px-4 py-2 text-pampas-100">
+              Low to High
+            </button>
+            <button className="cursor-not-allowed bg-mineral-green-600 px-4 py-2 text-pampas-100">
+              High to Low
+            </button>
+            <button className="cursor-not-allowed bg-mineral-green-600 px-4 py-2 text-pampas-100">
+              Rating
+            </button>
+          </div>
         </div>
+        <ProductGrid products={products} />
       </div>
     </div>
   );
