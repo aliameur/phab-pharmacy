5d9f9623cd42b69375094bf3095b8d4d24c1b193
Ali Ameur <aliameur169@gmail.com>
2024-01-08 02:18:34 +0000

    ci: pure heroku scripts

--- apps/backend/medusa-config.js
+++ apps/backend/medusa-config.js
@@ -29,14 +29,8 @@ const ADMIN_CORS =
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || 'http://localhost:8000';
 
-const DB_USERNAME = process.env.DB_USERNAME
-const DB_PASSWORD = process.env.DB_PASSWORD
-const DB_HOST = process.env.DB_HOST
-const DB_PORT = process.env.DB_PORT
-const DB_DATABASE = process.env.DB_DATABASE
-
 const DATABASE_URL =
-  `postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`
+  process.env.DATABASE_URL || 'postgres://localhost/medusa-starter-default';
 
 const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379';
 
