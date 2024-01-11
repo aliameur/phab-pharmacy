aed53f5fdf4d1c48fd92e5a550ec193690967391
Rubén Moreno aguado <rmorenoaguado3643@gmail.com>
2024-01-11 18:19:12 +0000

    Add ThemeButton component to Navbar

--- apps/admin/app/components/layout/Navbar.tsx
+++ apps/admin/app/components/layout/Navbar.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import React from 'react';
 
 import SidebarToggle from './SidebarToggle';
+import ThemeButton from './ThemeButton';
 
 export default function Navbar() {
   return (
@@ -14,22 +15,8 @@ export default function Navbar() {
       </div>
 
       <div className="flex-none">
-        <p className="px-8 text-sm">Admin Dashboard</p>
-        <button className="btn btn-square btn-ghost">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            className="inline-block h-5 w-5 stroke-current"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-            ></path>
-          </svg>
-        </button>
+        <p className="text-sm px-2">Admin Dashboard</p>
+        <ThemeButton />
       </div>
     </div>
   );
