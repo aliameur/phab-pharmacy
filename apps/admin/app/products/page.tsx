2123818bb9c28882e85adebcbfc8544a37ba5802
Rubén Moreno aguado <rmorenoaguado3643@gmail.com>
2024-01-11 16:31:34 +0000

    Refactor products page to use new components and fetch products by category handle

--- apps/admin/app/products/page.tsx
+++ apps/admin/app/products/page.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
-import Layout from '../components/Layout';
+import Layout from '../components/layout/Layout';
+import ProductsViewer from '../components/products/ProductsViewer';
+import { getCategories, getProductsByCategoryHandle } from '@phab/data-next';
 
-export default function ProductsPage() {
+export default async function ProductsPage() {
+
+  const categories = await getCategories();
+  const productsPromises = categories.map(category => 
+    getProductsByCategoryHandle(category.handle)
+  );
+  const products = await Promise.all(productsPromises);
+
   return (
     <Layout>
-      <></>
+      <ProductsViewer products={products} categories={categories} />
     </Layout>
   );
 }
