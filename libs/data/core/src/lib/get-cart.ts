c3aa0e460b38ae9d043f1a2f8aab09f7ba34c720
Ali Ameur <aliameur169@gmail.com>
2024-01-12 16:15:36 +0000

    feat(data): make cart fetches not cacheable

--- libs/data/core/src/lib/get-cart.ts
+++ libs/data/core/src/lib/get-cart.ts
@@ -3,6 +3,7 @@ import { MedusaCartOperation, TRequestFn } from '../types';
 export const getCart = (requestFn: TRequestFn) => async (cartId: string) => {
   const res = await requestFn<MedusaCartOperation>({
     path: `/carts/${cartId}`,
+    cache: 'no-cache',
   });
 
   return res.body.cart;
