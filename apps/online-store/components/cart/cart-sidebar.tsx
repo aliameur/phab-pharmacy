b7686613750f6ac5d2e7a86309299868deaf77f8
Ali Ameur <aliameur169@gmail.com>
2024-01-13 04:29:41 +0000

    style(online-store): remove unused fragment

--- apps/online-store/components/cart/cart-sidebar.tsx
+++ apps/online-store/components/cart/cart-sidebar.tsx
@@ -43,20 +43,18 @@ export const CartSidebar = ({ cart }: TCartSidebar) => {
       side="right"
       className="flex flex-col gap-4 bg-mineral-green-600 px-4 py-5 sm:max-w-md"
       outerIcon={({ openSidebar }) => (
-        <>
-          <button
-            onClick={openSidebar}
-            aria-label="Open cart sidebar"
-            className="relative p-2"
-          >
-            {cartTotal ? (
-              <div className="absolute -right-0.5 -top-0.5 flex h-5 w-5 items-center justify-center rounded-full bg-mineral-green-600 text-xs font-medium text-pampas-100">
-                {cartTotal}
-              </div>
-            ) : null}
-            <ShoppingCart className="h-6 w-6 text-mineral-green-600" />
-          </button>
-        </>
+        <button
+          onClick={openSidebar}
+          aria-label="Open cart sidebar"
+          className="relative p-2"
+        >
+          {cartTotal ? (
+            <div className="absolute -right-0.5 -top-0.5 flex h-5 w-5 items-center justify-center rounded-full bg-mineral-green-600 text-xs font-medium text-pampas-100">
+              {cartTotal}
+            </div>
+          ) : null}
+          <ShoppingCart className="h-6 w-6 text-mineral-green-600" />
+        </button>
       )}
     >
       {({ closeSidebar }) => (
