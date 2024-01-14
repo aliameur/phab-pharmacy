6cb29fdf75c15f162d09d0badf105f3f73b5682b
Rubén Moreno aguado <rmorenoaguado3643@gmail.com>
2024-01-14 20:24:12 +0000

    fix: Refactor page component to use useEffect hook for redirect logic

--- apps/admin/app/page.tsx
+++ apps/admin/app/page.tsx
@@ -1,13 +1,16 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Index() {
   const router = useRouter();
 
-  if (localStorage.getItem('apiToken')) {
-    router.push('/products');
-  } else {
-    router.push('/login');
-  }
+  useEffect(() => {
+    if (localStorage.getItem('apiToken')) {
+      router.push('/products');
+    } else {
+      router.push('/login');
+    }
+  });
 }
