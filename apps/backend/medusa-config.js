6fb6ac29e52785108f703d94486526cc0e20e56e
at421 <at421@ic.ac.uk>
2024-01-14 06:59:24 +0000

    feat: image hosting on medusa

--- apps/backend/medusa-config.js
+++ apps/backend/medusa-config.js
@@ -46,7 +46,8 @@ const plugins = [
   {
     resolve: `@medusajs/file-local`,
     options: {
-      upload_dir: 'uploads',
+      upload_dir: 'uploads/images',
+      backend_url: 'https://phab-pharmacy-backend-ab775283aa48.herokuapp.com'
     },
   },
   {
